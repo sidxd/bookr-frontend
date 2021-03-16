7bff2a5334fbaa2d3ca1dd3cec00b88222f0445a
Carl Hansen <carlhansen.design@gmail.com>
2021-03-16 13:15:50 +0800

    :fire: removed unused files.

--- src/front-end/js/main.js
+++
@@ -1,5 +0,0 @@
-const closeButton = document.querySelector(".close__btn")
-const addButton = document.querySelector(".action__button")
-
-// Close Pop-up.
-closeButton.addEventListener("click", () => window.close())
