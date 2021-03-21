2921775ffd28429dfc8b10a43d2c2e519db67db3
Sid <slatxyo@gmail.com>
2021-03-21 22:44:17 +0530

    :recycle: small refactor for js.

--- src/front-end/js/WindowActions.js
+++ src/front-end/js/WindowActions.js
@@ -2,22 +2,20 @@
 
 // Add site function.
 
-const siteListArr = []
+// let x = (siteListArr) => siteListArr.filter((v, i) => siteListArr.indexOf(v) === i)
 
-const header = document.querySelector(".header")
-const actionButtons = document.querySelector(".action-buttons")
-const searchbar = document.querySelector(".searchbar")
-const navbar = document.querySelector(".navbar")
+const getElement = (classname) => document.querySelector(classname)
+const makeElement = (element) => document.createElement(element)
 
 handleDelete = () => {
 
     if(document.querySelector('.delete-container')) return
 
-    const deleteContainer = document.createElement("div")
-    const deleteText = document.createElement("p")
-    const deleteOptions = document.createElement("nav")
-    const deleteConfirm = document.createElement("button")
-    const deleteCancel = document.createElement("button")
+    const deleteContainer = makeElement("div")
+    const deleteText = makeElement("p")
+    const deleteOptions = makeElement("nav")
+    const deleteConfirm = makeElement("button")
+    const deleteCancel = makeElement("button")
 
     deleteContainer.classList.add("delete-container")
     deleteText.classList.add("delete-text")
@@ -35,7 +33,7 @@ handleDelete = () => {
     deleteCancel.innerHTML = "No"
     deleteConfirm.innerHTML = "Yes"
 
-    const ghostbox = document.querySelector(".ghostbox")
+    const ghostbox = getElement(".ghostbox")
     ghostbox.style.pointerEvents = "none"
     ghostbox.classList.add("darker")
 
@@ -44,6 +42,7 @@ handleDelete = () => {
         setTimeout(() => {
             siteList.removeChild(siteBox)
         }, 200)
+        siteBox.classList.remove("box-out")
         document.body.removeChild(deleteContainer)
         ghostbox.style.pointerEvents = "all"
         ghostbox.classList.remove("darker")
@@ -56,21 +55,18 @@ handleDelete = () => {
     })
 }
 
-removeBox = document.createElement("button") //....... Create "remove" button.
-removeBox.classList.add("site-remove")
-removeBox.style.display = "none"
+let titleStore
 
-const addSite = () => {
+addSite = () => {
 
-    siteList = document.querySelector(".sites-list") //.. List of all bookmarked sites.
-    siteBox = document.createElement("li") //............. Create the container for a bookmark.
-    const siteTitle = document.createElement("h5") //........... Create title of the site.
-    const expandButton = document.createElement("button") //.... Create button that expands bookmark.
-    const siteDesc = document.createElement("p") //............. Create description (To be changed).
-    const siteCapture = document.createElement("img") //.......... Create image screenshot.
-    // removeBox = document.querySelector("site-remove") //....... Create "remove" button.
-    const siteFavicon = document.createElement("img")
-    let siteUrl = ''
+    siteList = getElement(".sites-list") //.. List of all bookmarked sites.
+    siteBox = makeElement("li") //............. Create the container for a bookmark.
+    siteTitle = makeElement("h5") //........... Create title of the site.
+    expandButton = makeElement("button") //.... Create button that expands bookmark.
+    siteDesc = makeElement("p") //............. Create description (To be changed).
+    siteCapture = makeElement("img") //.......... Create image screenshot.
+    removeBox = makeElement("site-remove") //....... Create "remove" button.
+    siteFavicon = makeElement("img")
 
     siteBox.classList.add("site")
     siteTitle.classList.add("site-title")
@@ -78,6 +74,7 @@ const addSite = () => {
     siteDesc.classList.add("site-desc")
     siteCapture.classList.add("site-img")
     siteFavicon.classList.add("site-favicon")
+    removeBox.classList.add("site-remove")
     removeBox.style.display = "block"
 
     siteBox.appendChild(siteTitle)
@@ -86,39 +83,61 @@ const addSite = () => {
     siteBox.appendChild(siteCapture)
     siteBox.appendChild(removeBox)
     siteBox.appendChild(siteFavicon)
-
     expandButton.innerHTML = `<svg width="12" height="7" viewBox="0 0 12 7" fill="none"><path d="M11 1L6 6L1 1" stroke="#696969" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>`
+    
+    // GET elements from Chrome's API.
 
-    chrome.tabs.captureVisibleTab(1, {}, (imageData) => siteCapture.src = imageData)
+    chrome.tabs.query ({
+        active : true,
+        currentWindow : true,
+        status : "complete",
+        windowType : "normal"
+        }, (tab) => {
 
-    siteBox.style.display = "flex"
+            siteTitle = siteTitle.innerHTML
+            siteTitle = tab[0].title
+            titleStore = tab[0].title
 
-    chrome.tabs.query({currentWindow: true, active: true}, // Can probably be smaller.
-    (tab) => {
-        siteTitle.innerHTML = tab[0].title
-        if(siteTitle.innerHTML.length > 16) {
-            siteTitle.innerHTML = `${siteTitle.innerHTML.substring(0, 14)}...`
+            // LIMIT characters in title to 16.
+
+            if(siteTitle.length > 16) {
+                siteTitle = `${siteTitle.substring(0, 16)}...`
+            }
+
+            siteFavicon.src = tab[0].favIconUrl
+            siteUrl = tab[0].url
+
+            // GET site image.
+
+            chrome.tabs.captureVisibleTab(1, {}, (imageData) => siteCapture.src = imageData)
+
+            // GET description.
+
+            const { id: tabId } = tab[0].url;
+      
+            let code = `document.querySelector("meta[name=description]").getAttribute("content")`
+
+            chrome.tabs.executeScript(tabId, { code }, (result) => {
+
+                siteDesc.innerHTML = result[0]
+
+                // LIMIT characters in description to 64.
+
+                if(siteDesc.innerHTML.length > 64) {
+                    siteDesc.innerHTML = `${siteDesc.innerHTML.substring(0, 64)}...`
+                }
+            })
+
         }
-        siteUrl = tab[0].url
-    })
+    )
 
-    chrome.tabs.query({
-        "active": true, //fetch active tabs
-        "currentWindow": true, //fetch tabs in current window
-        "status": "complete", //fetch completely loaded windows
-        "windowType": "normal" //fetch normal windows
-    }, function (tabs) {
-        for (tab in tabs) {
-            siteFavicon.src = tabs[tab].favIconUrl// Use this URL as needed
-        }
-    })
+    console.log(titleStore)
+
+    // CREATE and open link in new tab when title is clicked on.
 
     siteTitle.addEventListener("click", () => {
         chrome.tabs.create({url: siteUrl, active: false})
     })
-
-    siteList.prepend(siteBox)
-    siteListArr.push(siteBox)
 
     expandButton.addEventListener("click", () => {
         siteCapture.classList.toggle("full-view")
@@ -127,28 +146,10 @@ const addSite = () => {
         removeBox.classList.toggle("site-remove__expanded")
     })
 
-    chrome.tabs.query (
-        { active: true },
-        function(tabs) {
-          const { id: tabId } = tabs[0].url;
-      
-          let code = `
-          document.querySelector("meta[name=description]").getAttribute("content")
-          `
-          chrome.tabs.executeScript(tabId, { code }, function (result) {
-            console.log(result[0])
-            siteDesc.innerHTML = result[0]
-            if(siteDesc.innerHTML.length > 64) {
-                siteDesc.innerHTML = `${siteDesc.innerHTML.substring(0, 64)}...`
-            }
-          })
-        }
-    )
+    removeBox.addEventListener("click", () => {
+        handleDelete()
+    })
 }
-
-removeBox.addEventListener("click", () => {
-    handleDelete()
-})
 
 addButton.addEventListener("click", () => {
     addSite()
@@ -156,10 +157,10 @@ addButton.addEventListener("click", () => {
 
 // Handle button events on navbar.
 
-const actionsContainer = document.querySelector(".action-buttons")
-const searchButton = document.querySelector(".search")
-const recentsButton = document.querySelector(".recent")
-const searchBar = document.querySelector(".searchbar")
+const actionsContainer = getElement(".action-buttons")
+const searchButton = getElement(".search")
+const recentsButton = getElement(".recent")
+const searchBar = getElement(".searchbar")
 
 searchButton.addEventListener("click", () => {
     searchButton.classList.add("active__link")
